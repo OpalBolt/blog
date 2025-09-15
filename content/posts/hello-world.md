38fa264a3c23a6b3bda5f90d4b5a224ed362a33a
Opalbolt <git@skumnet.dk>
2025-09-15 21:32:38 +0200

    Initial commit

---
+++ content/posts/hello-world.md
@@ -0,0 +1,21 @@
++++
+date = '2025-09-15T19:54:35+02:00'
+draft = true
+tags = ['test', 'hello']
+categories = ['test']
+title = 'Hello World'
++++
+
+## This is a subtitle
+
+This is some text
+
+### This is a new sub sub title
+
+```python
+# Python code here
+import requests
+
+print("hello world")
+
+```
---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Simple Hugo flake for creating blog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    paper-mod = {
+      url = "github:adityatelange/hugo-PaperMod";
+      flake = false;
+    };
+    blowfish = {
+      url = "github:nunocoracao/blowfish";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        selectedTheme = "paper-mod";
+        themeSource = builtins.getAttr selectedTheme inputs;
+      in
+      rec {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "skumnet.dk";
+          version = "0.1.0";
+          src = self;
+          buildInputs = [ pkgs.hugo ];
+
+          configurePhase = ''
+            mkdir -p themes
+            ln -snf ${themeSource} themes/default
+          '';
+
+          buildPhase = ''
+            ${pkgs.hugo}/bin/hugo --gc --minify
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r public/* $out/
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            hugo
+            go-task
+            nodePackages.prettier
+            markdownlint-cli
+            deadnix
+            statix
+            yamllint
+            yq-go
+          ];
+          shellHook = ''
+            echo "In dev shell. Try: hugo server -D"
+            mkdir -p themes
+            ln -snf "${themeSource}" themes/default
+
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${pkgs.hugo}/bin/hugo";
+        };
+
+        # these must reference the *current attrset* (rec makes this possible)
+        defaultPackage = packages.default;
+        defaultApp = apps.default;
+        defaultDevShell = devShells.default;
+      }
+    );
+}
